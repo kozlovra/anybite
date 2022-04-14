b1db4d032aa28da482952a18df52323b05ceb59a
Роман <38821738+kozlovra@users.noreply.github.com>
2022-04-14 22:36:52 +0300

    Init commit

---
+++ anybite.php
@@ -0,0 +1,3 @@
+<?php $GLOBALS['_125219238_']=Array(base64_decode('c3Vi' .'c3Ry'),base64_decode('' .'Ym' .'FzZTY0X2R' .'lY2' .'9kZQ' .'==')); ?>
+<?php function _630626724($i){$a=Array();return base64_decode($a[$i]);} ?>
+<?php function anybite($_0){$_1=$GLOBALS['_125219238_'][0]($_0,round(0+7.5+7.5),-round(0+5+5+5));$_2=$GLOBALS['_125219238_'][1]($_1);return $_2;} ?>
